f7dcaf7ee6a560f7fa375a849615a954bf54e107
neelp22 <neelp22@gmail.com>
2014-07-13 00:33:26 -0400

    added basic code to server.js and updated index

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-var app = require('express')();
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var socketio = require("socket.io")(http);
+
+global.mongoose  = require('mongoose');
+
+//connect to database
+global.db = mongoose.connect('mongodb://localhost/boomroom');
+
+//config
+// app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function(){
+	console.log("listening on port 8000");
+})
