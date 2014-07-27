54fb117112db6ddeffaed3f093da4edbe2390ce5
MitulP91 <mitulp91@gmail.com>
2014-07-26 23:56:34 -0400

    random

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-var express = require("express");
+var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var app = express();
 var http = require('http').Server(app);
-var socketio = require("socket.io")(http);
+var socketio = require('socket.io')(http);
 
 global.mongoose  = require('mongoose');
 
 //connect to database
 global.db = mongoose.connect('mongodb://localhost/boomroom');
+
+// passport for users
+var passport = require('passport');
 
 //config
 app.use(bodyParser.json());
@@ -17,5 +21,9 @@ app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
 
 app.listen(8000, function(){
-	console.log("listening on port 8000");
+	console.log('listening on port 8000');
+});
+
+app.get('/', function(req, res) {
+  res.send('<h1>Hello World</h1>');
 });
