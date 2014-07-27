60a09977576e2fd6f5034ddc964befc78c71669b
neelp22 <neelp22@gmail.com>
2014-07-27 01:06:15 -0400

    added models and collections for users, songs, and rooms

---
+++ public/javascripts/backbone/models/Song.js
@@ -0,0 +1,6 @@
+var Elevatrapp = Elevatrapp || { Models: {}, Collections: {}, Views: {} };
+
+Backbone.Model.prototype.idAttribute = "_id";
+
+Elevatrapp.Models.Song = Backbone.Model.extend({
+});
---
+++ public/javascripts/backbone/models/User.js
@@ -0,0 +1,6 @@
+var Elevatrapp = Elevatrapp || { Models: {}, Collections: {}, Views: {} };
+
+Backbone.Model.prototype.idAttribute = "_id";
+
+Elevatrapp.Models.User = Backbone.Model.extend({
+});
