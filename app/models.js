d53b0c1b7a0fcac12ba1566608de7b29ba833bff
MitulP91 <mitulp91@gmail.com>
2014-07-27 00:23:14 -0400

    add user schema

--- app/models.js
+++ app/models.js
@@ -26,6 +26,8 @@ var songSchema = mongoose.Schema({
 
 // User Schema
 var userSchema = mongoose.Schema({
-  username: String,
-  email: 
+  username: {type: String, required: true, index: { unique: true } },
+  email: {type: String, required: true, index: { unique: true } },
+  password: {type: String, required: true },
+  favorite_songs: [songSchema]
 });
