0eeecd25a2d367207cd58dd3417a4fcf97372bb2
MitulP91 <mitulp91@gmail.com>
2014-07-27 01:15:06 -0400

    change index.html to jade, add backbone scripts, add bootstrap css

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/controllers'));
 
 app.listen(8000, function(){
 	console.log('listening on port 8000');
