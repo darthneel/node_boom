0ae8b7384f36b4e4eb2e131357b54ecaf9e335be
neelp22 <neelp22@gmail.com>
2014-07-27 00:28:27 -0400

    updated user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,11 @@
+var songSchema = require(__dirname + '/models/song.js');
+
+// app/models/user.js
+// load the things we need
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+// define the schema for our user model
 // User Schema
 var userSchema = mongoose.Schema({
   username: {type: String, required: true, index: { unique: true } },
@@ -5,3 +13,18 @@ var userSchema = mongoose.Schema({
   password: {type: String, required: true },
   favorite_songs: [songSchema]
 });
+
+
+// methods ======================
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
